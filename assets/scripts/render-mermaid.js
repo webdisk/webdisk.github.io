48d9a359e7d936a594ac6e83a3240d9311740eb2
webdisk <webdisk@users.noreply.github.com>
2020-05-10 18:10:05 +0900

    Fix space

--- assets/scripts/render-mermaid.js
+++ assets/scripts/render-mermaid.js
@@ -12,4 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
         pre.parentElement.replaceChild(div, pre);
     }
     mermaid.render(id, graphDefinition, insertSvg);
-  }});
+  }
+});
