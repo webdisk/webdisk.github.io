8c729bb9ecfa664d888c358c433f665ebb5d02dc
webdisk <webdisk@users.noreply.github.com>
2020-05-10 18:29:42 +0900

    Update

--- _includes/scripts/render-mermaid.js
+++ _includes/scripts/render-mermaid.js
@@ -1,5 +1,6 @@
 import * as mermaid from 'https://unpkg.com/mermaid@8.5.0/dist/mermaid.min.js';
 
+console.log(mermaid)
 mermaid.initialize({ startOnLoad: false });
 
 document.addEventListener('DOMContentLoaded', () => {
